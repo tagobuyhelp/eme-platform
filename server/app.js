1132d3093c4e0c1a1e373509a06a9b6c32eb4dea
Tagobuy <tagobuyhelp@gmail.com>
2026-06-02 06:51:38 +0530

    ...

--- server/app.js
+++ server/app.js
@@ -39,7 +39,7 @@ const clientDistPath = path.join(__dirname, "../client/dist");
 if (fs.existsSync(clientDistPath)) {
   app.use(express.static(clientDistPath));
 
-  app.get("*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(clientDistPath, "index.html"));
   });
 }
