2df1f68d5873c989b12cf5aa72c4957353c7a2a0
Tagobuy <tagobuyhelp@gmail.com>
2026-06-02 08:26:35 +0530

    ..

--- server/modules/certificate/certificate.service.js
+++ server/modules/certificate/certificate.service.js
@@ -136,8 +136,13 @@ export async function generateCertificatePDF(data) {
 
   try {
     browser = await puppeteer.launch({
-      headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      headless: "new",
+      args: [
+        "--no-sandbox", 
+        "--disable-setuid-sandbox", 
+        "--disable-dev-shm-usage", 
+        "--disable-gpu"
+      ],
     });
 
     const page = await browser.newPage();
